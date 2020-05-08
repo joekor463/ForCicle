dc8e200123145b6046e71d45348f575aeb7701bb
Evgeny Korobenkov <joekor463@gmail.com>
2020-05-08 22:17:33 +0800

    forcicle

---
+++ ForCicle.java
@@ -0,0 +1,16 @@
+public class ForCicle {
+    public static void main(String[] args){
+        int sum = 0;
+        for(int i = 1; i <= 1000; i++){
+            int k = i;
+            if ((k%3) > 0){
+                if ((k%5 > 0)) {
+
+                }
+            }
+            sum = sum + k;
+        }
+        System.out.println ( "Summa =" + sum );
+    }
+
+}
